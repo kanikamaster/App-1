b0613fce9d8d6cf2cb837a8ad589508ccfb31c38
kanikamaster <matataku08@gmail.com>
2025-07-24 14:11:07 +0900

    初回

---
+++ firebase.example.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+export default firebaseConfig;
