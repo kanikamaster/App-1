b99c1a91ae280d3f39770577f78ee9b7488d9528
kanikamaster <matataku08@gmail.com>
2025-07-24 13:16:15 +0900

    firebase変更

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,13 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmjcLOLhejjuyBT5V5r3FlXK1JMvD412c",
-  authDomain: "defeat-task.firebaseapp.com",
-  projectId: "defeat-task",
-  storageBucket: "defeat-task.firebasestorage.app",
-  messagingSenderId: "205613498218",
-  appId: "1:205613498218:web:640ec9b8ee475a2be06f13"
+  apiKey: "FAKE_API_KEY_FOR_PUBLIC",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "0000000000",
+  appId: "1:0000000000:web:fakeappid123",
 };
 
 const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-export { db };
